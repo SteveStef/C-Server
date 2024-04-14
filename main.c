3dde901e4e05b65865db8ad03768b2edc1154785
Stephen Stefanatos <stephenstef456@gmail.com>
2024-04-14 15:42:52 -0400

    Updates

--- main.c
+++ main.c
@@ -8,122 +8,135 @@
 #include <netinet/in.h>
 
 #define MAX_CLIENTS 100
-#define PORT 8080
+#define PORT 9000
 #define BUFFER_SIZE 1024
 
-typedef struct {
-  int sock;
-  char *name;
-} Client;
-
-Client client_sockets[MAX_CLIENTS];
+int client_sockets[MAX_CLIENTS];
 int client_count = 0;
-pthread_mutex_t client_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void printClients() {
-  printf("Clients: [");
-  for (int i = 0; i < client_count; i++) {
-    printf("%s: %i", client_sockets[i].name, client_sockets[i].sock);
+  printf("Clients: [ ");
+  for (int i = 0; i < MAX_CLIENTS; i++) {
+    if (client_sockets[i] != -1) {
+      printf("%d ", client_sockets[i]);
+    }
   }
   printf("]\n");
+}
+
+void addClient(int client) {
+  pthread_mutex_lock(&lock);
+  for (int i = 0; i < MAX_CLIENTS; i++) {
+    if (client_sockets[i] == -1) {
+      client_sockets[i] = client;
+      client_count++;
+      break;
+    }
+  }
+  pthread_mutex_unlock(&lock);
+}
+
+void removeClient(int client) {
+  pthread_mutex_lock(&lock);
+  for (int i = 0; i < MAX_CLIENTS; i++) {
+    if (client_sockets[i] == client) {
+      client_sockets[i] = -1;
+      break;
+    }
+  }
+  client_count--;
+  pthread_mutex_unlock(&lock);
+}
+
+void sendToAllClients(char *message, int socket) {
+  for (int i = 0; i < MAX_CLIENTS; i++) {
+    if (client_sockets[i] != -1 && client_sockets[i] != socket) {
+      write(client_sockets[i], message, strlen(message));
+    }
+  }
 }
 
 void *connection_handler(void *socket_desc) {
   int sock = *(int*)socket_desc;
   char buffer[BUFFER_SIZE] = {0};
+
   while (1) {
     int n = read(sock, buffer, BUFFER_SIZE);
 
     if (n <= 0) {
-      pthread_mutex_lock(&client_mutex);
-      for (int i = 0; i < client_count; i++) {
-        if (client_sockets[i].sock == sock) {
-          client_sockets[i].sock = -1;
-          client_sockets[i].name = NULL;
-          client_count--;
-          break;
-        }
-      }
-
-      pthread_mutex_unlock(&client_mutex);
+      removeClient(sock);
       printf("Client disconnected\n");
       printClients();
       break;
     }
 
     printf("Received message: %s\n", buffer);
-
-    pthread_mutex_lock(&client_mutex);
-    for (int i = 0; i < client_count; i++) {
-      if (client_sockets[i].sock != sock && client_sockets[i].sock != -1) {
-        write(client_sockets[i].sock, buffer, strlen(buffer));
-      }
-    }
-    pthread_mutex_unlock(&client_mutex);
+    sendToAllClients(buffer, sock);
     memset(buffer, 0, BUFFER_SIZE);
   }
-  
+
   close(sock);
+  pthread_exit(0);
   return 0;
 }
 
 
 int main() {
-  int server_socket, new_socket;
+  int sockfd, new_socket;
   struct sockaddr_in address;
+
   int opt = 1;
   int addrlen = sizeof(address);
 
   for (int i = 0; i < MAX_CLIENTS; i++) {
-    Client tmp;
-    tmp.sock = -1;
-    tmp.name = NULL;
-    client_sockets[i] = tmp;
+    client_sockets[i] = -1;
   }
 
-  server_socket = socket(AF_INET, SOCK_STREAM, 0);
-  setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
 
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons(PORT);
 
-  int bindConn = bind(server_socket, (struct sockaddr *)&address, sizeof(address));
+  int bindConn = bind(sockfd, (struct sockaddr *)&address, sizeof(address));
   if (bindConn < 0) {
     perror("bind failed");
     return 1;
   }
 
-  listen(server_socket, MAX_CLIENTS);
+  listen(sockfd, MAX_CLIENTS);
   printf("Server listening on port %d\n", PORT);
 
   while (1) {
     printf("Waiting for connection...\n");
-    new_socket = accept(server_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-    printf("Connection accepted\n");
-
     if (client_count < MAX_CLIENTS) {
-      Client new_client;
-      new_client.sock = new_socket;
-      new_client.name = "Client";
-      for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (client_sockets[i].sock == -1) {
-          client_sockets[i] = new_client;
-          break;
-        }
+      new_socket = accept(sockfd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+
+      if (new_socket < 0) {
+        perror("accept failed");
+        continue;
       }
-      client_count++;
-      //client_sockets[client_count++] = new_client;
+
+      printf("Connection accepted\n");
+      addClient(new_socket);
       printClients();
-    }
 
-    pthread_t client_thread;
-    if (pthread_create(&client_thread, NULL, connection_handler, (void*)&new_socket) < 0) {
-      perror("could not create thread");
-      return 1;
+      pthread_t client_thread;
+      int *new_socket_ptr = malloc(sizeof(int));
+      *new_socket_ptr = new_socket;
+
+      if (pthread_create(&client_thread, NULL, connection_handler, (void*)new_socket_ptr) < 0) {
+        perror("could not create thread");
+        free(new_socket_ptr); // Free memory on fail
+        removeClient(new_socket);
+        return 1;
+      }
+      pthread_detach(client_thread);
+    } else {
+      printf("Max clients reached\n");
     }
-    pthread_detach(client_thread);
   }
-
   return 0;
 }
